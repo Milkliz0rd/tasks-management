550133a25913bfa8465df567661621a29ca6ba44
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2025-09-05 11:27:37 +0200

    Exercice 3: Debugging

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,15 @@ Route::group(['prefix' => 'tasks'], function () {
 
     Route::get('/{id}', [TaskController::class, 'showById']); // route to get task by id, use laravel method to found controller with class
 
+    /**
+    * The route was previously: Route::get('/', [TaskController::class, 'create']);
+     * It contained two issues:
+     * 1. The HTTP method was wrong. It should be POST, not GET, when creating a resource.
+     * 2. The controller method was wrong. It should be 'store', not 'create'.
+     */
+    Route::post('/', [TaskController::class, 'store']); 
+
     /** end */
 
-    Route::get('/', [TaskController::class, 'create']);
-    Route::delete('/{task}', [TaskController::class, 'destroy']);
+     Route::delete('/{task}', [TaskController::class, 'destroy']);
 });
